7b9085c9d07eff82c4462a6867908629ee6ae916
jack.li <jack.li@eisoo.com>
2019-08-10 09:43:49 +0800

    增加leetcode 8 的解决方案，修改leetcode4和7的问题

--- src/leetcode/python3/leetcode4.py
+++ src/leetcode/python3/leetcode4.py
@@ -52,43 +52,6 @@ class Solution:
         else:
             return (nums[len(nums) // 2 - 1] + nums[len(nums) // 2]) / 2
 
-# 2 二分法
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-
-        if len(nums1) > len(nums2):
-            return self.findMedianSortedArrays(nums2, nums1)
-
-        m, n = len(nums1), len(nums2)
-        imin = 0
-        imax = m
-        while imin <= imax:
-            i = (imin + imax) // 2
-            j = (m + n + 1) // 2 - i
-            if j != 0 and i != m and nums2[j - 1] > nums1[i]:
-                imin += 1
-            elif i != 0 and j != n and nums1[i - 1] > nums2[j]:
-                imax -= 1
-            else:
-                max_left = 0
-                if i == 0:
-                    max_left = nums2[j - 1]
-                elif j == 0:
-                    max_left = nums1[i - 1]
-                else:
-                    max_left = max(nums1[i - 1], nums2[j - 1])
-                if (m + n) % 2:
-                    return max_left
-
-                min_right = 0
-                if i == m:
-                    min_right = nums2[j]
-                elif j == n:
-                    min_right = nums1[i]
-                else:
-                    min_right = min(nums1[i], nums2[j])
-
-                return (max_left + min_right) / 2
-
 
 #  140ms 二分法，奇怪的是速度优势并不是很明显
 class Solution2:
--- src/leetcode/python3/leetcode7.py
+++ src/leetcode/python3/leetcode7.py
@@ -5,27 +5,24 @@
     倒排数字，数字是4个字节有符号的整数，如果倒排的数字超过范围则为0
 """
 
-from functools import reduce
 
-# 非常朴素的思路
+# 非常朴素的思路,40ms
 class Solution:
     def reverse(self, x: int) -> int:
         if -10 < x < 10:
             return x
-        d = []
         sign = 1
+        reverse = 0
         if x < 0:
             sign = -1
             x = -x
         while x:
-            t = x % 10
-            d.append(t)
+            reverse = reverse * 10 + x % 10
             x //= 10
-        r = reduce(lambda x, y: x * 10 + y, d)
         if sign == 1:
-            if r > (1 << 31) - 1:
+            if reverse > (1<<31)-1:
                 return 0
         else:
-            if r > 1 << 31:
+            if reverse > 1<<31:
                 return 0
-        return sign * r
+        return sign * reverse

df1e5be02e994f87c8e785477982cd182b7e1c68
jack.li <jack.li@eisoo.com>
2019-08-14 19:02:21 +0800

    增加leetcode 211 2种解决方案

--- src/leetcode/python3/leetcode211.py
+++ src/leetcode/python3/leetcode211.py
@@ -48,3 +48,85 @@ class WordDictionary:
             if "$" in q:
                 return True
         return False
+
+
+# 使用深度优先策略 412ms
+class WordDictionary2:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = dict()
+
+    def addWord(self, word: str) -> None:
+        """
+        Adds a word into the data structure.
+        """
+        tempt_root = self.root
+        for s in word:
+            if s not in tempt_root:
+                tempt_root[s] = dict()
+            tempt_root = tempt_root[s]
+        if "$" not in tempt_root:
+            tempt_root["$"] = dict()
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+        """
+        tempt_root = self.root
+
+        def dfs(input_s, iroot):
+            if len(input_s) == 0:
+                if "$" in iroot:
+                    return True
+                return False
+
+            fs = input_s[0]
+            if fs == ".":
+                result = []
+                for _, v in iroot.items():
+                    result.append(dfs(input_s[1:], v))
+                return any(result)
+            elif fs in iroot:
+                return dfs(input_s[1:], iroot[fs])
+            else:
+                return False
+
+        return dfs(word, tempt_root)
+
+# 按照字符串长度进行分类比较 188ms
+from collections import defaultdict
+class WordDictionary3:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = defaultdict(list)
+
+    def addWord(self, word: str) -> None:
+        """
+        Adds a word into the data structure.
+        """
+        n_len = len(word)
+        self.root[n_len].append(word)
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+        """
+        t_len = len(word)
+
+        for wd in self.root[t_len]:
+            result = 0
+            for i, s in enumerate(word):
+                if s == "." or wd[i] == s:
+                    result += 1
+                else:
+                    break
+            if result == t_len:
+                return True
+
+        return False

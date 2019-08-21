bda227323268222385901436bf0eae62d7693279
xixici <202388990@qq.com>
2019-08-21 21:54:47 +0800

    ♌ xixici  push at 2019-08-21 21:54:47

---
+++ src/leetcode/python/148-sort-list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Author  :   https://github.com/xixici
+Date    :   2019/8/21
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """

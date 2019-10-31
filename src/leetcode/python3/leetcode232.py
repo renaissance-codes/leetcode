2d22ca46a66ef367b0db670381069a92a14262c2
jack.li <jack.li@eisoo.com>
2019-10-31 17:42:33 +0800

    增加 leetcode 232 方案2

--- src/leetcode/python3/leetcode232.py
+++ src/leetcode/python3/leetcode232.py
@@ -37,3 +37,46 @@ class MyQueue:
         Returns whether the queue is empty.
         """
         return len(self.stack1) == 0
+
+
+# 方法2
+class MyQueue2:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.stack1.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+
+        return self.stack2.pop()
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+
+        return self.stack2[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return len(self.stack1) == 0 and len(self.stack2) == 0

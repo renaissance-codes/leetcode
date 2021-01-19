500a8c9fa188afdfd2c91c729bce41021071f1a2
xixici <202388990@qq.com>
2021-01-19 22:15:00 +0800

    🔯 xixici  push at 2021-01-19 22:15:00

---
+++ LeetCodeTencent/S020.java
@@ -0,0 +1,56 @@
+package LeetCodeTencent;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * @author Yang Lei
+ * @date 2021/1/19
+ */
+public class S020 {
+    public static void main(String[] args) {
+        S020 s020 = new S020();
+        System.out.println(s020.isValid("(("));
+    }
+
+    public boolean isValid(String s) {
+        if (s.length() < 2) {
+            return false;
+        }
+        ArrayList<Character> chars = new ArrayList<>();
+        chars.add('[');
+        chars.add('{');
+        chars.add('(');
+
+        Stack<Character> strings = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (chars.contains(c)) {
+                strings.add(c);
+            } else {
+                Character pop = strings.pop();
+                Character pairChar = getPairChar(pop);
+                if (c == pairChar) {
+
+                } else {
+                    return false;
+                }
+            }
+        }
+        if(strings.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+
+    private Character getPairChar(char c) {
+        if (c == '{') {
+            return '}';
+        } else if (c == '(') {
+            return ')';
+        } else if (c == '[') {
+            return ']';
+        }
+        return ' ';
+    }
+}

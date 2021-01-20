19ac7e015bbb39d8779a21a02ce044fe6dfd757b
xixici <202388990@qq.com>
2021-01-20 20:15:01 +0800

    ♊ xixici  push at 2021-01-20 20:15:00

--- LeetCodeTencent/S206.java
+++ LeetCodeTencent/S206.java
@@ -1,16 +1,43 @@
 package LeetCodeTencent;
 
+import java.util.List;
+import java.util.Stack;
+
 /**
  * @author Yang Lei
  * @date 2021/1/20
  */
 public class S206 {
     public ListNode reverseList(ListNode head) {
+        ListNode listNode = new ListNode();
+        Stack<Integer> integers = new Stack<>();
+        while (head != null){
+            integers.add(head.val);
+            head = head.next;
+        }
+        while(!integers.isEmpty()){
+            Integer pop = integers.pop();
+            ListNode listNode1 = new ListNode();
+            listNode1.val = pop;
+            listNode.next = listNode1;
+            listNode.next = listNode;
 
+        }
+        return listNode;
     }
 
     public static void main(String[] args) {
-
+        S206 s206 = new S206();
+        ListNode listNode = new ListNode();
+        ListNode listNode2 = new ListNode();
+        ListNode listNode3 = new ListNode();
+        listNode.val = 3;
+        listNode2.val = 2;
+        listNode3.val = 0;
+        listNode.next = listNode2;
+        listNode2.next = listNode3;
+        ListNode listNode1 = s206.reverseList(listNode);
+        System.out.println(listNode1.toString());
     }
 
 }

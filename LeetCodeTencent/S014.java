ef4e85a0b86c4c37fec47773e441ee2d4b373fde
xixici <202388990@qq.com>
2021-01-20 15:15:00 +0800

    ⛎ xixici  push at 2021-01-20 15:15:00

---
+++ LeetCodeTencent/S014.java
@@ -0,0 +1,17 @@
+package LeetCodeTencent;
+
+/**
+ * @author Yang Lei
+ * @date 2021/1/20
+ */
+public class S014 {
+
+    public String longestCommonPrefix(String[] strs) {
+
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}

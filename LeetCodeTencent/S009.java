4246ba259058a80189bda4fb6f5e2a2cdcf92bc4
xixici <202388990@qq.com>
2021-01-20 12:15:00 +0800

    ♌ xixici  push at 2021-01-20 12:15:00

--- LeetCodeTencent/S009.java
+++ LeetCodeTencent/S009.java
@@ -5,29 +5,33 @@ package LeetCodeTencent;
  * @date 2021/1/20
  */
 public class S009 {
+    public static void main(String[] args) {
+        S009 s009 = new S009();
+        System.out.println(s009.isPalindrome(1000030001));
+    }
+
     public boolean isPalindrome(int x) {
-        if (x < 11) {
+        if (x < 0) {
             return false;
         }
+        if (x >= 0 && x < 10) {
+            return true;
+        }
         String s = String.valueOf(x);
-        for (int i = 0; i < s.length(); i++) {
-            String s1 = String.valueOf(s.charAt(i));
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            String s1 = String.valueOf(s.charAt(0));
 
             if (!s.endsWith(s1)) {
                 return false;
-            }else{
-                if(s.length()==2){
+            } else {
+                if (s.length() <= 2) {
                     return true;
                 }
             }
-            s = s.substring(i + 1, s.length() - 1);
+            s = s.substring(1, s.length() - 1);
         }
         return true;
-    }
-
-    public static void main(String[] args) {
-        S009 s009 = new S009();
-        System.out.println(s009.isPalindrome(2112));
     }
 
 

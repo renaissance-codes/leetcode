6d5620e8a3bc2eb969db147dd23f6d69019f2d64
xixici <202388990@qq.com>
2021-01-21 18:15:00 +0800

    ♊ xixici  push at 2021-01-21 18:15:00

--- LeetCodeTencent/S206.java
+++ LeetCodeTencent/S206.java
@@ -26,30 +26,31 @@ public class S206 {
     }
     public ListNode reverseList(ListNode head) {
         ListNode listNode = new ListNode();
+        ListNode cur0 = listNode;
+
         ListNode cur1 = head;
-        listNode = cur1;
-        listNode.next = null;
-        cur1 = cur1.next;
-        while (head != null) {
-            head.next = listNode;
-            listNode = head;
-            head = head.next;
+        while (cur1 != null) {
+            cur0.next = cur1;
+            cur1 = cur1.next;
         }
-        return listNode.next;
+        return cur0.next;
     }
 
     public static void main(String[] args) {
         S206 s206 = new S206();
-        ListNode listNode = new ListNode();
+        ListNode listNode1 = new ListNode();
         ListNode listNode2 = new ListNode();
         ListNode listNode3 = new ListNode();
-        listNode.val = 3;
+        ListNode listNode4 = new ListNode();
+        listNode1.val = 1;
         listNode2.val = 2;
-        listNode3.val = 0;
-        listNode.next = listNode2;
+        listNode3.val = 3;
+        listNode4.val = 4;
+        listNode1.next = listNode2;
         listNode2.next = listNode3;
-        ListNode listNode1 = s206.reverseList(listNode);
-        System.out.println(listNode1.toString());
+        listNode3.next = listNode4;
+        ListNode listNode = s206.reverseList(listNode1);
+        System.out.println(listNode.toString());
     }
 
 }

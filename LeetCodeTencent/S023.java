166889a8fcf8ad82c2eff11e6882c7d5a16510a2
xixici <202388990@qq.com>
2021-01-25 20:15:00 +0800

    ♊ xixici  push at 2021-01-25 20:15:00

--- LeetCodeTencent/S023.java
+++ LeetCodeTencent/S023.java
@@ -4,21 +4,13 @@ package LeetCodeTencent;
  * @author Yang Lei
  * @date 2021/1/25
  */
-public class S021 {
-    public static void main(String[] args) {
-        ListNode listNode = new ListNode(1);
-        ListNode listNode1 = new ListNode(2);
-        ListNode listNodea = new ListNode(1);
-        ListNode listNodea1 = new ListNode(2);
-        ListNode listNode2 = new ListNode(3);
-        ListNode listNodea2 = new ListNode(4);
-        listNode.next = listNode1;
-        listNode1.next = listNode2;
-        listNodea.next = listNodea1;
-        listNodea1.next = listNodea2;
-
-        S021 s021 = new S021();
-        s021.mergeTwoLists(listNode,listNodea);
+public class S023 {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode res = new ListNode(Integer.MIN_VALUE);
+        for (int i = 0; i < lists.length; i++) {
+            res = mergeTwoLists(lists[i], res);
+        }
+        return res.next;
     }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {

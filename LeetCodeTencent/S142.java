8bdd45d6e1cc24e88dc8c97240eeca823fd531c2
xixici <202388990@qq.com>
2021-01-25 16:15:00 +0800

    ⛎ xixici  push at 2021-01-25 16:15:00

--- LeetCodeTencent/S142.java
+++ LeetCodeTencent/S142.java
@@ -4,28 +4,29 @@ package LeetCodeTencent;
  * @author Yang Lei
  * @date 2021/1/25
  */
-public class S141 {
+public class S142 {
     public static void main(String[] args) {
+
         ListNode listNode = new ListNode(1);
         ListNode listNode1 = new ListNode(2);
         ListNode listNode2 = new ListNode(3);
         listNode.next = listNode1;
         listNode1.next = listNode2;
         listNode2.next = listNode1;
-        S141 s141 = new S141();
-        System.out.println(s141.hasCycle(listNode));
+        S142 s142 = new S142();
+        System.out.println(s142.detectCycle(listNode));
     }
 
-    public boolean hasCycle(ListNode head) {
-        ListNode head2 = head;
-        while (head2 != null  && head2.next != null) {
+    public ListNode detectCycle(ListNode head) {
+        ListNode head2 = head.next;
+        while (head2 != null && head2.next != null) {
             head = head.next;
             head2 = head2.next.next;
             if (head.equals(head2)) {
-                return true;
+                return head.next;
             }
         }
-        return false;
+        return null;
 
     }
 }
